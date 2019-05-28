77b0b3c8e934f9f29bf425e4e782db321a870b3a
Alex Joke <alexjoke@inbox.ru>
2019-05-28 23:59:52 +0300

    initial commit

---
+++ headfirst/__init__.py
@@ -0,0 +1,18 @@
+movies = ["The holy grail",
+          "The life of brain"]
+
+print(movies)
+print(movies[1])
+
+movies.append("annymals")
+movies.extend(["bana", "list"])
+
+print(movies)
+
+movies.pop(3)
+
+print(movies)
+
+movies.remove("list")
+
+print(movies)
