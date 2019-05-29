85ee39fd6234725e0351b8e6274b661815119bcc
Alex Joke <alexjoke@inbox.ru>
2019-05-29 09:14:06 +0300

    loops & conditions

--- headfirst/__init__.py
+++ headfirst/__init__.py
@@ -1,5 +1,6 @@
 movies = ["The holy grail",
-          "The life of brain"]
+          "The life of brain",
+          ["embed list"]]
 
 print(movies)
 print(movies[1])
@@ -16,3 +17,24 @@ print(movies)
 movies.remove("list")
 
 print(movies)
+
+print("--------------------------------")
+
+for iterator in movies:
+    if isinstance(iterator, list):
+        for nested_iter in iterator:
+            print(nested_iter)
+    else:
+        print(iterator)
+
+count = 0
+
+while count < len(movies):
+    print(movies[count])
+    count = count+1
+
+print(isinstance(movies, list))
+print(isinstance(count, list))
+
+
+
