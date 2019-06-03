8d17e26ee9ef62f46c1526c940e50c8870835dd4
Alex Joke <alexjoke@inbox.ru>
2019-06-03 22:53:29 +0300

    Refactor chapter5

--- headfirst/python/chapter5/chapter5.py
+++ headfirst/python/chapter5/chapter5.py
@@ -1,26 +1,14 @@
-with open('james.txt') as jaf:
-    data = jaf.readline()
-james = data.strip().split(',')
+# Return first line from a given file
+def parse_file(file):
+    try:
+        with open(file) as f:
+            data = f.readline()
+        return data.strip().split(',')
 
-with open('julie.txt') as juf:
-    data = juf.readline()
-julie = data.strip().split(',')
+    except FileNotFoundError as e:
+        print("File not found!" + e)
 
-with open('mikey.txt') as mif:
-    data = mif.readline()
-mikey = data.strip().split(',')
-
-with open('sarah.txt') as saf:
-    data = saf.readline()
-sarah = data.strip().split(',')
-
-print(sorted(james))
-print(sorted(julie))
-print(sorted(mikey))
-print(sorted(sarah))
-
-print('-----------------------------------------------------')
-
+# Return a time in format `min.sec`
 def sanitize(time_string):
     if '-' in time_string:
         splitter = '-'
@@ -33,7 +21,14 @@ def sanitize(time_string):
 
     return(mins + '.' + secs)
 
-print(sorted([sanitize(item) for item in james]))
-print(sorted([sanitize(item) for item in julie]))
-print(sorted([sanitize(item) for item in mikey]))
-print(sorted([sanitize(item) for item in sarah]))
+# Create new lists from files
+james = parse_file('james.txt')
+julie = parse_file('julie.txt')
+mikey = parse_file('mikey.txt')
+sarah = parse_file('sarah.txt')
+
+# Print top 3 values from the sorted set of unified items
+print(sorted(set([sanitize(item) for item in james]))[0:3])
+print(sorted(set([sanitize(item) for item in julie]))[0:3])
+print(sorted(set([sanitize(item) for item in mikey]))[0:3])
+print(sorted(set([sanitize(item) for item in sarah]))[0:3])
