a7c092ff1f87a15e37d027937f07568d17355575
Alex Joke <alexjoke@inbox.ru>
2019-06-07 16:50:47 +0300

    Pyhon classes

--- headfirst/python/chapter6/chapter6.py
+++ headfirst/python/chapter6/chapter6.py
@@ -1,3 +1,24 @@
+class AtheleteList(list):
+    def __init__(self, a_name, a_dob=None, a_times=[]):
+        list.__init__([])
+        self.name = a_name
+        self.dob = a_dob
+        self.extend(a_times)
+        
+
+    def top3(self):
+        return (sorted(set([sanitize(t) for t in self]))[0:3])
+
+def get_coach_data(filename):
+    try: 
+        with open(filename) as f:
+            data = f.readline()
+            temp = data.strip().split(',')
+            return (AtheleteList(temp.pop(0),temp.pop(0),temp))
+    except IOError as e:
+        print('File error: ' + str(e))
+        return(None)
+
 def sanitize(time_string):
     if '-' in time_string:
         splitter = '-'
@@ -8,11 +29,6 @@ def sanitize(time_string):
     (mins, secs) = time_string.split(splitter)
     return(mins + '.' + secs)
 
-def get_coach_data(filename):
-    try: 
-        with open(filename) as f:
-            data = f.readline()
-        return(data.strip().split(','))
-    except IOError as e:
-        print('File error: ' + str(e))
-        return(None)
+
+sarah = get_coach_data('sarah2.txt')
+print(sarah.name + "'s fastest times are: " + str(sarah.top3()))
